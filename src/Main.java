8e7403bb8816ccef8b2c664423b3da2d3fd2efd6
likai <33309776+Leekailklk@users.noreply.github.com>
2018-05-22 11:57:22 +0800

    test fetch
    test fetch

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Test Git");
         System.out.println("Test push");
+        System.out.println("Test fetch");
          System.out.println("Test pull");
     }
 }
